2411928a990f40b5ad2d746eeead7aea786308e3
bronte000@kaist.ac.kr <bronte000@kaist.ac.kr>
2020-11-09 18:01:48 +0900

    inventory editing

--- Assets/02. Scripts/Louis_Menu.cs
+++ Assets/02. Scripts/Louis_Menu.cs
@@ -6,11 +6,13 @@ public class Louis_Menu : MonoBehaviour
 {
 
     public GameObject ActionMenu;
+    public GameObject Inventory;
 
     // Start is called before the first frame update
     void Start()
     {
         ActionMenu.SetActive(false);
+        Inventory.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,6 +28,18 @@ public class Louis_Menu : MonoBehaviour
             ActionMenu.SetActive(false);
         } else {
             ActionMenu.SetActive(true);
+        }
+    }
+
+    public void Click_Bag_Button()
+    {
+        if (Inventory.activeSelf == true)
+        {
+            Inventory.SetActive(false);
+        }
+        else
+        {
+            Inventory.SetActive(true);
         }
     }
 
