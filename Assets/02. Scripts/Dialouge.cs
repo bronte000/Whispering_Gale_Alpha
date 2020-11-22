ea2bb60c112fdd45d6848a186da81ab87d841496
unknown <aleron@kaist.ac.kr>
2020-11-22 22:02:59 +0900

    start dialouge

---
+++ Assets/02. Scripts/Dialouge.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialouge
+{
+    public string name;
+
+    [TextArea(3, 10)] //minimum & maximum number of lines TextArea will use
+    public string[] sentences;
+}
