d0deeb54f02624977b8f967259d7b7b2b7c7e261
unknown <aleron@kaist.ac.kr>
2020-11-23 02:05:51 +0900

    basic dialogue

--- Assets/02. Scripts/Dialogue.cs
+++ Assets/02. Scripts/Dialogue.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Dialouge
+public class Dialogue
 {
     public string name;
 
-    [TextArea(3, 10)] //minimum & maximum number of lines TextArea will use
+    [TextArea(3, 30)] //minimum & maximum number of lines TextArea will use
     public string[] sentences;
 }
