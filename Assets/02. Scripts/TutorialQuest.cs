53ce7bd50d7210a61362f63121d5c76563e02b80
unknown <aleron@kaist.ac.kr>
2020-11-30 01:16:25 +0900

    dood

---
+++ Assets/02. Scripts/TutorialQuest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialQuest : MonoBehaviour
+{
+    public GameObject tutorial;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tutorial.GetComponent<QuestTrigger>().quest.hasStarted == true)
+            ActivateQuestCompletedFunc();
+    }
+
+    void ActivateQuestCompletedFunc()
+    {
+        Debug.Log("QuestCompleteActivated");
+        tutorial.GetComponent<Button>().onClick.AddListener(QuestDone);
+    }
+
+    void QuestDone()
+    {
+        tutorial.GetComponent<QuestTrigger>().quest.nextAction.nextObject.GetComponent<DialogueTrigger>().TriggerDialogue();
+        tutorial.GetComponent<QuestTrigger>().quest.hasStarted = false;
+    }
+
+}
