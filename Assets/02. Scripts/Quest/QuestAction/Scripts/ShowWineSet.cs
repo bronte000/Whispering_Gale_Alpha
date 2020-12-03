3a9034b2a0b459ca8dc13a1f5122a9d40bd2f161
bronte000@kaist.ac.kr <bronte000@kaist.ac.kr>
2020-12-03 17:45:49 +0900

    upper room background done

---
+++ Assets/02. Scripts/Quest/QuestAction/Scripts/ShowWineSet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "QuestAction/UpperRoom/ShowWineSet")]
+public class ShowWineSet : QuestAction
+{
+    public override bool ExecuteRole()
+    {
+        GameObject.Find("WineSet").SetActive(true);
+        return true;
+    }
+}
