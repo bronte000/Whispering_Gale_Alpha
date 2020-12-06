bfaf3ad4beed824192b0b19ac50464e7bcc859d8
unknown <aleron@kaist.ac.kr>
2020-12-06 16:13:35 +0900

    start quest11

---
+++ Assets/02. Scripts/Dialogue/ColliderDialogueTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderDialogueTrigger : MonoBehaviour
+{
+    public GameObject dialogue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Louis")
+            dialogue.GetComponent<DialogueTrigger>().TriggerDialogue();
+    }
+}
