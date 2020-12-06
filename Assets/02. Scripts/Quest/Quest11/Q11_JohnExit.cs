30d57fbdf28edf1f06d51d3148404f7949cb4afe
unknown <aleron@kaist.ac.kr>
2020-12-07 01:04:39 +0900

    john

---
+++ Assets/02. Scripts/Quest/Quest11/Q11_JohnExit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "QuestAction/Quest11/JohnExit")]
+public class Q11_JohnExit : QuestAction
+{
+    public GameObject john;
+    public DialogueTrigger dialogue5;
+
+    public override bool ExecuteRole()
+    {
+        // John walks away
+
+        // Execute Dialogue5
+        dialogue5.TriggerDialogue();
+
+        return true;
+    }
+}
