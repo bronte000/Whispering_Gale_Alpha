cdd1a13aa67d93855f5b0055b9f3ec832a1952ba
unknown <aleron@kaist.ac.kr>
2020-12-07 01:59:10 +0900

    turnturn

--- Assets/02. Scripts/Quest/Quest11/ColliderDialogueTrigger.cs
+++ Assets/02. Scripts/Quest/Quest11/ColliderDialogueTrigger.cs
@@ -6,14 +6,25 @@ public class ColliderDialogueTrigger : MonoBehaviour
 {
     public GameObject dialogue;
     public string player_name; //임시용
-    private int file_number;
+    private GameObject louis;
+
+    void Start()
+    {
+        louis = GameObject.Find("Louis");
+        PlayerPrefs.DeleteKey(PlayerPrefs.GetInt(player_name).ToString() + "_quest11");
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Louis")
         {
             if ((!PlayerPrefs.HasKey(PlayerPrefs.GetInt(player_name).ToString() + "_quest11")) || PlayerPrefs.GetInt(PlayerPrefs.GetInt(player_name).ToString() + "_quest11") == 0)
+            {
+                Vector3 newDirection = transform.position - louis.transform.position;
+                transform.rotation = Quaternion.LookRotation(-newDirection);
                 dialogue.GetComponent<DialogueTrigger>().TriggerDialogue();
+            }
+                
         }
         Debug.Log(PlayerPrefs.GetInt(PlayerPrefs.GetInt(player_name).ToString() + "_quest11"));
     }
